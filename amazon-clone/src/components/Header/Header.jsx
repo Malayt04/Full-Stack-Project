d66afe269db1defe50e5eef51249f4a46a6caad2
Malay Tiwari <malayt04@gmail.com>
2023-12-31 08:57:26 +0530

    amazon clone started

---
+++ amazon-clone/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './header.css'
+
+function Header() {
+  return (
+    <div className='header'>
+      <img className='header-logo' src='https://assets.aboutamazon.com/88/05/0feec6ff47bab443d2c82944bb09/amazon-logo.png' />
+      <div className='header-search'>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Header
