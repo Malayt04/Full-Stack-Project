6c61755c63fec0c99ee8cceb8590eba706b8c129
Malay Tiwari <malayt04@gmail.com>
2023-12-31 18:42:35 +0530

    asdf

--- amazon-clone/src/components/Home/Home.jsx
+++ amazon-clone/src/components/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './home.css'
+import Product from '../Product/Product'
 
 
 function Home() {
@@ -7,6 +8,22 @@ function Home() {
     <div className='home'>
       <div className='home-container'>
         <a href='https://www.primevideo.com/' target='blank'><img className='home-image' src='https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg' alt='Amazon Prime'/></a>
+      </div>
+      <div className='home-row'>
+        <Product/>
+        {/*Product*/ }
+      </div>
+      <div className='home-row'>
+        {/*Product*/ }
+        {/*Product*/ }
+      </div>
+      <div className='home-row'>
+        {/*Product*/ }
+        {/*Product*/ }
+      </div>
+      <div className='home-row'>
+        {/*Product*/ }
+        {/*Product*/ }
       </div>
     </div>
   )
